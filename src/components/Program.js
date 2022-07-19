c084c3f170fea28d1bd3222c3d4b5398b2bd001e
thethmuenaing <thethmuenaing22112002@gmail.com>
2022-07-19 22:18:40 +0630

    adding the code of program component

--- src/components/Program.js
+++ src/components/Program.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React from "react";
+
+//import icons
+import PhoneIcon from "../assets/img/phone.svg";
 
 const Program = () => {
-  return <div>Program</div>;
+	return (
+		<div>
+			{/* phone icon & phone number */}
+			<div className="flex lg:gap-x-[10px] items-baseline justify-end">
+				<img
+					className="w-[28px] h-[28px] lg:w-[34px] lg:h-[34px]"
+					src={PhoneIcon}
+					alt="/"
+				/>
+				<div className="lg:text-[23px] text-blue font-extrabold">
+					+ 1000 10 84 00
+				</div>
+			</div>
+			{/* open hours */}
+			<div className="text-sm">
+				{/* show only in desktop mode */}
+				<div className="hidden lg:flex">
+					Opening Hours: Mon - Sun: 10am - 6pm
+				</div>
+				{/* show only in mobile mode */}
+				<div className="lg:hidden text-right">Mon - Sun: 10am - 6pm</div>
+			</div>
+		</div>
+	);
 };
 
 export default Program;
