8a8fe4e484910e2e5a44fdcf4432d31d7ebf826e
thethmuenaing <thethmuenaing22112002@gmail.com>
2022-07-19 09:47:25 +0630

    layerout of Header

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from "react";
 
 //import components
 import Hero from "./components/Hero";
+import Header from "./components/Header";
 
 const App = () => {
 	return (
 		<div className="w-full max-w-[1440px] mx-auto bg-orange-quaternary relative">
+			<Header />
 			<Hero />
+			<div className="h-[2000px]"></div>
 		</div>
 	);
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+
+//import Logo
+import Logo from "../assets/img/logo.svg";
+
+//import icons
+import { CgMenuRight } from "react-icons/cg";
+
+//import components
+import Nav from "./Nav";
+import NavMobile from "./NavMobile";
+import Program from "./Program";
 
 const Header = () => {
-  return <div>Header</div>;
+	//header bg state
+	const [bg, setBg] = useState(false);
+
+	//scroll event
+	window.addEventListener("scroll", () => {
+		window.scrollY > 50 ? setBg(true) : setBg(false);
+	});
+
+	return (
+		<header
+			className={`${
+				bg ? "bg-white py-3 shadow-md" : "bg-none py-5"
+			} fixed w-full right-0 left-0 z-10 transition-all duration-300`}
+		>
+			<div className="container mx-auto flex justify-between items-center">
+				{/* logo */}
+				<a href="#">
+					<img src={Logo} alt="/" />
+				</a>
+				{/* nav */}
+				<div className="hidden lg:flex">
+					<Nav />
+				</div>
+				{/* Program */}
+				<Program />
+				{/* nav mobile button */}
+				<div className="lg:hidden cursor-pointer">
+					<CgMenuRight className="text-blue text-3xl" />
+				</div>
+				{/* mobile nav */}
+				<div className="hidden">
+					<NavMobile />
+				</div>
+			</div>
+		</header>
+	);
 };
 
 export default Header;
